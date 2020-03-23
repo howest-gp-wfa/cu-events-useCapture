ba696dbc0e3eab617fe6f6378b5623afa7789cd1
Vercaemer Stef <stefaan.vercaemer@howest.be>
2020-03-23 11:44:51 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,64 @@
+"use strict";
+
+var divFeedback; 
+var divBuiten, divMidden,divBinnen;
+var slcCapture;
+var teller;
+
+window.addEventListener('load',Initieer);
+
+function Initieer(){
+
+  KoppelElementen();
+  KoppelEvents();
+
+  slcCapture.addEventListener('change', KoppelEvents);
+
+}
+
+const KoppelEvents = () => {
+  
+  let captureSelectie = (slcCapture.options[slcCapture.selectedIndex].value == 'true');
+  
+  divBuiten.removeEventListener('click',GeefKlik, !captureSelectie);
+  divMidden.removeEventListener('click',GeefKlik, !captureSelectie);
+  divBinnen.removeEventListener('click',GeefKlik, !captureSelectie);  
+  
+  divBuiten.addEventListener('click',GeefKlik,captureSelectie);
+  divMidden.addEventListener('click',GeefKlik,captureSelectie);
+  divBinnen.addEventListener('click',GeefKlik,captureSelectie);
+
+  divFeedback.innerHTML = "";
+}
+
+const KoppelElementen = () => {
+  divFeedback = document.getElementById("divFeedback");
+  divBuiten = document.querySelector('#divBuiten');
+  divMidden = document.querySelector('#divMidden');
+  divBinnen = document.querySelector('#divBinnen');
+  slcCapture = document.getElementById("slcCapture");
+}
+
+const KlikBuiten = () => {
+  divFeedback.innerHTML += `Je klikte op Buiten <br/>`; 
+}
+
+const KlikMidden = () => {
+  divFeedback.innerHTML += `Je klikte op Midden <br/>`; 
+}
+
+const KlikBinnen = () => {
+  divFeedback.innerHTML += `Je klikte op Binnen <br/>`; 
+}
+
+function GeefKlik(e){
+  let propagationStopped = (slcPropagate.options[slcPropagate.selectedIndex].value == 'true');
+   divFeedback.innerHTML += `Je klikte op ${this.id} <br/>`; 
+    if (propagationStopped) e.stopPropagation();
+}
+
+
+
+
+
+
